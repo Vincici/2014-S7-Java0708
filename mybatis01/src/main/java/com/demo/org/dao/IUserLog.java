b1b2f4e44be9825bf379833f27efaea707be4de2
lxl2016 <lxlxl2016@outlook.com>
2017-09-19 14:55:45 +0800

    annotation mapping

---
+++ mybatis01/src/main/java/com/demo/org/dao/IUserLog.java
@@ -0,0 +1,12 @@
+package com.demo.org.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Select;
+
+import com.demo.org.bean.UserLog;
+
+public interface IUserLog {
+	@Select("select * from user_log")
+	public List<UserLog> getAll();
+}
--- mybatis01/src/main/java/com/demo/org/mybatis01/App.java
+++ mybatis01/src/main/java/com/demo/org/mybatis01/App.java
@@ -11,7 +11,9 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import com.demo.org.bean.Account;
 import com.demo.org.bean.Role;
+import com.demo.org.bean.UserLog;
 import com.demo.org.dao.IRoleDao;
+import com.demo.org.dao.IUserLog;
 
 /**
  * Hello world!
@@ -27,21 +29,28 @@ public class App
         SqlSession session = ssf.openSession();
         
         
-        //第一种映射方式
+        //第一种映射方式 域名查找
         List<Account> accountList = session.selectList("test.getAll");
         
         for(Account account:accountList) {
         	System.out.println(account.getUsername());
         }
         System.out.println("--------------------");
-        //第二种映射方式
+        //第二种映射方式 接口映射
         
         IRoleDao ird = session.getMapper(IRoleDao.class);
         List<Role> roleList = ird.getAll();
         for(Role r : roleList) {
         	System.out.println(r.getRolename());
         }
-        
-        
+        System.out.println("-------------------");
+        //第三种映射方式 注解配置
+        	//添加映射配置
+        session.getConfiguration().addMapper(IUserLog.class);
+        IUserLog iul = session.getMapper(IUserLog.class);
+        List<UserLog> userLogList = iul.getAll();
+        for(UserLog ul : userLogList) {
+        	System.out.println(ul.getDescription());
+        }
     }
 }
